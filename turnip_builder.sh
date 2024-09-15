40db9f40e59cc4982fa96b92221f45b21122f802
Adnan Syamsafa <137246570+eikarna@users.noreply.github.com>
2024-09-15 10:26:19 +0700

    Update turnip_builder.sh

--- turnip_builder.sh
+++ turnip_builder.sh
@@ -5,7 +5,7 @@ nocolor='\033[0m'
 deps="meson ninja patchelf unzip curl pip flex bison zip"
 workdir="$(pwd)/turnip_workdir"
 magiskdir="$workdir/turnip_module"
-ndkver="android-ndk-r27"
+ndkver="android-ndk-r27b"
 sdkver="33"
 clear
 
